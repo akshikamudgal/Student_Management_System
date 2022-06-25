dcc81f5877243032463ca37c4401c7868cbf17f5
Akshika Mudgal <66902249+akshikamudgal@users.noreply.github.com>
2022-06-25 14:52:59 +0530

    Create birthdayParty.cpp

---
+++ COLLEGE/birthdayParty.cpp
@@ -0,0 +1,10 @@
+//preceeding and successding is less than and more than 2 in each case
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+    cin>>n;
+    int out = n*(2*n-1);
+    cout<<out;
+}

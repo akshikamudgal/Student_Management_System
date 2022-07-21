93cf860d3e00cbe9ce4ecd1663a5e10ab50bb2e0
Akshika Mudgal <66902249+akshikamudgal@users.noreply.github.com>
2022-07-21 13:39:08 +0530

    Create MajorityEle.cpp

---
+++ COLLEGE/map/MajorityEle.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int majorityElement(vector<int>& nums)
+{
+  int size=nums.size();
+  unordered_map<int,int>m;
+	// counting each number from the array using hashmap:
+  for(auto i:nums)
+  {
+    m[i]++;
+  }
+	// check if the number's frequency is greater than half size of the array and return it's value immediately:
+  for(auto i:m)
+  {
+    if(i.second>size/2)
+    {
+      return i.first;
+    }
+  }
+  return 0;
+}
+int main()
+{
+  int n1;
+  cin>>n1;
+  vector<int>n;
+  int a;
+  for(int i=0;i<n1;i++)
+  {
+    cin>>a;
+    n.push_back(a);
+  }
+  cout<<majorityElement(n);
+}
